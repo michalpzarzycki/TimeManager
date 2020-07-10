668275fa4b4c30615c69a318d38960935f6de9e1
Michal <michal.p.zarzycki@wp.pl>
2020-07-10 04:14:36 -0700

    Added positioning to Route1 Component

--- src/components/Route1.js
+++ src/components/Route1.js
@@ -3,5 +3,16 @@ import styles from './Route1.module.css'
 
 
 export default function Route1() {
-    return <div className={styles.route1Container}>Route1</div>
+    return <div className={styles.route1Container}>
+        <section className={styles.route1Charts}>
+            <div className={styles.route1ChartFirst}>FIRST CHART</div>
+            <div className={styles.route1ChartSecond}>SECOND CHART</div>
+            <div className={styles.route1ChartThird}>THIRD CHART</div>
+        </section>
+        <section className={styles.route1TasksNav}>
+            <div className={styles.route1TasksNav_searchDiv}>SEARCH</div>
+            <div className={styles.route1TasksNav_createDiv}>CREATE</div>
+        </section>
+        <section className={styles.route1TasksSection}>TASK SECTION</section>
+    </div>
 } 
