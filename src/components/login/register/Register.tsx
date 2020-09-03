4c92fafe79278040f0c00a61466355e23a20c193
Michal <michal.p.zarzycki@wp.pl>
2020-09-03 06:23:04 -0700

    Fixed styles in Register

--- src/components/login/register/Register.tsx
+++ src/components/login/register/Register.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import styles from './Register.module.css';
 import firebase, { db, storage } from '../../../firebase/firebase'
-import Login from './Login';
 import TextInput from './Inputs/TextInput';
 import FileInput from './Inputs/FileInput';
 import { useRegisterValidate } from '../../../hooks/useRegisterValidate'
@@ -21,39 +20,37 @@ const INIT_STATE = {
     file:[]
             }
 interface IUser {
-    email : any,
-    password: any,
-    repeatPassword: any,
-    name: any,
-    surname: any,
-    nickname: any,
-    telephone: any,
-    city: any,
-    country: any,
-    description: any,
+    email : string,
+    password: string,
+    repeatPassword: string,
+    name: string,
+    surname: string,
+    nickname: string,
+    telephone: string,
+    city: string,
+    country: string,
+    description: string,
     file: any
 }
 
 export default function Register() {
+    //State of all user data from register input
     const [user, setUser] = useState<IUser>({...INIT_STATE});
-    const { errors, isValidate } = useRegisterValidate(user)
-    let [buttonDisabled, setButtonDisabled] = useState(true)
+    //Returning all error messages from from validation and isValidate boolean
+    const { errors, isValidate } = useRegisterValidate(user);
+    //State of disabled button(in case of not pass validation)
+    let [buttonDisabled, setButtonDisabled] = useState(true);
+    //State of loading button(validation is ok, form is sending to firebase)
     let [buttonLoading, setButtonLoading] = useState(false)
     let [file, setFile] = useState<any>('')
     
 
     function handleChange(e : any) {
-        console.log(e.target.value)
         setUser({...user, [e.target.name]: e.target.value})
-        console.log("ERORS", errors)
-        console.log("IS VALIDATE", isValidate)
         isValidate ? setButtonDisabled(false) : setButtonDisabled(true)
     }
     function handleFileChange(e : any) {
-        console.log("jsahfkas", e.target.files[0])
         setUser({...user, 'file': e.target.files[0].type})
-        console.log("ERORS", errors)
-        console.log("IS VALIDATE", isValidate)
         setFile(e.target.files[0])
         isValidate ? setButtonDisabled(false) : setButtonDisabled(true)
     }
@@ -65,18 +62,13 @@ export default function Register() {
                 setButtonLoading(false)
             }).catch(e => {
                 setButtonLoading(false)
-                console.log("ERROR", e)
             })
             storage.ref().child(`profiles/${user.email}.jpg`).put(file).then(() => {
-                console.log("POSZEDL")
             }).catch((err) => {
 
             })
         } else {
         }
-      
-       
-    
         db.collection("users").add({
             email: user.email,
             password: user.password,
@@ -86,33 +78,78 @@ export default function Register() {
             telephone: user.telephone,
             city:user.city,
             country: user.country,
-            description: user.description,
-
-
-
-        })
-        .then(function() {
+            description: user.description})
+        .then(() => {
             console.log("Document successfully written!");
         })
-        .catch(function(error) {
+        .catch((error) => {
             console.error("Error writing document: ", error);
         });
     }
     return <div className={styles.mainDiv}>
       <h1 className={styles.header}>REGISTER FORM</h1>
         <form className={styles.form}>
-            <TextInput type="email" placeholder="email" handleChange={handleChange} name="email"/>
-            <TextInput error={errors.password} type="password" placeholder="password" handleChange={handleChange} name="password"/>
-            <TextInput error={errors.repeatPassword} placeholder="ResetPassword" type="password" handleChange={handleChange} name="repeatPassword"/>
-            <TextInput error={errors.name} placeholder="Name" handleChange={handleChange} name="name"/>
-            <TextInput error={errors.surname} placeholder="Surname" handleChange={handleChange} name="surname"/>
-            <TextInput error={errors.nickname} placeholder="Nickname" handleChange={handleChange} name="nickname"/>
-            <FileInput error={errors.file} handleFileChange={handleFileChange} name="file" file={file}/>
-            <TextInput error={errors.telephone} placeholder="Telephone" handleChange={handleChange} name="telephone"/>
-            <TextInput error={errors.city} placeholder="City" handleChange={handleChange} name="city"/>
-            <TextInput error={errors.country} placeholder="Country" handleChange={handleChange} name="country"/>
-            <textarea  placeholder="opis" name="description" onChange={handleChange}/>
-            <SubmitButton buttonDisabled={buttonDisabled} buttonLoading={buttonLoading} placeholder="SUBMIT" handleSubmit={handleSubmit} />
+            <TextInput 
+                type="email" 
+                placeholder="email" 
+                handleChange={handleChange} 
+                name="email"/>
+            <TextInput 
+                error={errors.password} 
+                type="password" 
+                placeholder="password" 
+                handleChange={handleChange} 
+                name="password"/>
+            <TextInput 
+                error={errors.repeatPassword} 
+                placeholder="ResetPassword" 
+                type="password" 
+                handleChange={handleChange} 
+                name="repeatPassword"/>
+            <TextInput 
+                error={errors.name} 
+                placeholder="Name" 
+                handleChange={handleChange} 
+                name="name"/>
+            <TextInput 
+                error={errors.surname} 
+                placeholder="Surname" 
+                handleChange={handleChange} 
+                name="surname"/>
+            <TextInput 
+                error={errors.nickname} 
+                placeholder="Nickname" 
+                handleChange={handleChange} 
+                name="nickname"/>
+            <FileInput 
+                error={errors.file} 
+                handleFileChange={handleFileChange} 
+                name="file" 
+                file={file}/>
+            <TextInput 
+                error={errors.telephone} 
+                placeholder="Telephone" 
+                handleChange={handleChange} 
+                name="telephone"/>
+            <TextInput 
+                error={errors.city}
+                placeholder="City" 
+                handleChange={handleChange} 
+                name="city"/>
+            <TextInput 
+                error={errors.country} 
+                placeholder="Country" 
+                handleChange={handleChange} 
+                name="country"/>
+            <textarea  
+                placeholder="opis" 
+                name="description" 
+                onChange={handleChange}/>
+            <SubmitButton 
+                buttonDisabled={buttonDisabled} 
+                buttonLoading={buttonLoading} 
+                placeholder="SUBMIT" 
+                handleSubmit={handleSubmit} />
         </form>
     </div>
 }
