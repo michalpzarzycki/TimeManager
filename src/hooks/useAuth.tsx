78d416b5ba578784a19ac1644d9c965bc90d9ffd
Michal <michal.p.zarzycki@wp.pl>
2020-09-04 03:06:59 -0700

    useAuth refactor

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,25 +1,27 @@
+//useAuth checked if there is any loggedIn user and return userData and loggedIn boolean
 import { useEffect, useState } from "react";
-
 import firebase from "../firebase/firebase";
 
 export const useAuth = () => {
   const [isLoggedIn, setIsLoggedIn] = useState<any>(null);
   const [user, setUser] = useState('')
   useEffect(() => {
-    let dataRef: any = firebase.auth()
-    dataRef.onAuthStateChanged(() => {
-      console.log("CHANGES")
-      console.log('isLOGGEDIN', isLoggedIn)
-      if (dataRef.currentUser) {
-          setUser(dataRef.currentUser)
-        setIsLoggedIn(true);
-        console.log("TRsUE", isLoggedIn)
 
+    let dataRef: any = firebase.auth()
+    //Watch if there is some Authentication Changes
+    dataRef.onAuthStateChanged(() => {
+      //if there is a logged user
+      if (dataRef.currentUser) {
+        //set user data
+        setUser(dataRef.currentUser)
+        //set that there is a logged user
+        setIsLoggedIn(true);
       } else {
+        //if there is no user make sure that it setIsLoggedIn must be false
         setIsLoggedIn(false);
       }
     });
   }, [user]);
-
-  return {isLoggedIn, user};
+//return info about who is or not logged
+  return { isLoggedIn, user };
 };
