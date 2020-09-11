507a8871c7efc0905f40ea55804fd32484aa43ca
Michal <michal.p.zarzycki@wp.pl>
2020-09-11 06:31:17 -0700

    Set post positioning

--- src/components/sidebarRoutes/community/TopicDiscuss.tsx
+++ src/components/sidebarRoutes/community/TopicDiscuss.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import styes from './TopicDiscuss.module.css';
+import styles from './TopicDiscuss.module.css';
 
 export default function TopicDiscuss() {
     return(
-        <div>DISCUSS</div>
+        <div>
+        <div className={styles.mainDiv}>
+            <div className={styles.userBox}>
+                <div className={styles.userNameBox}>userName</div>
+                <div className={styles.pictureBox}>IMG</div>
+                <div className={styles.infoBox}>infoBox</div>
+            </div>
+            <div className={styles.postBox}>
+                <div className={styles.messageBox}>
+                    <div className={styles.messageDate}>Posted 5 days ago</div>
+                    <div className={styles.messageContent}>JavaScript is the best programming language. Change my mind!</div>
+                </div>
+                <div className={styles.signatureBox}>
+                    <div className={styles.signatureContent}>The World Is Yours</div>
+                </div>
+            </div>
+        </div>
+        </div>
     )
 }
